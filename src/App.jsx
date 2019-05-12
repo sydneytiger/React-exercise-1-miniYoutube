4e80749f58217f0f0926477707b346cd8760c9c9
Tiger Chen <tigerchen@Tigers-MBP.gateway>
2019-05-12 10:23:36 +1000

    setup the app page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { keyword: null };
+  }
+
+  render() {
+    return (
+      <div>my mini youtube</div>
+    );
+  }
+
+}
+
+export default App;
