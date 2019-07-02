140922f7e97db797b27e6b41f02c556a3881f0d9
Tiger Chen <tiger.chen@ing.com.au>
2019-07-02 13:42:16 +1000

    allow full screen and updated web title

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -8,7 +8,7 @@ const VideoDetail = ({ video }) => {
         <div className="ui embed">
           <iframe
             src={`https://www.youtube.com/embed/${video.id.videoId}`}
-            title={video.snippet.title} />
+            title={video.snippet.title}  allowFullScreen/>
         </div>
         <div className="ui segment">
           <h4 className="ui header">{video.snippet.title}</h4>
